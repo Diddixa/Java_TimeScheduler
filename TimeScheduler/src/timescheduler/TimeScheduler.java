c8a55d15ad23041d055da787e788aa56b53f81b7
Oladjo <oladjo.biao@gmail.com>
2021-12-13 19:41:59 +0100

    test

--- TimeScheduler/src/timescheduler/TimeScheduler.java
+++ TimeScheduler/src/timescheduler/TimeScheduler.java
@@ -16,7 +16,7 @@ public class TimeScheduler {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        System.out.println("Geht!");
     }
     
 }
