62dac541bf57ff52db13a9fa1a5e7da979e0c99a
lam02 <lam02@LAPTOP-LPTEK7U7.fritz.box>
2021-12-13 20:49:29 +0100

    Test by Lam

--- TimeScheduler/src/timescheduler/TimeScheduler.java
+++ TimeScheduler/src/timescheduler/TimeScheduler.java
@@ -16,7 +16,7 @@ public class TimeScheduler {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Geht jetzt!");
+        System.out.println("Geht jetzt! mit LAM");
     }
     
 }
