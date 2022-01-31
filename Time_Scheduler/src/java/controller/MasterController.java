77e07da46fcf7ad1fe33d96018522a584db44cb1
Diddixa <djidde.sa@gmx.de>
2022-01-31 14:54:02 +0100

    Fixed creating a user issues

--- Time_Scheduler/src/java/controller/MasterController.java
+++ Time_Scheduler/src/java/controller/MasterController.java
@@ -24,30 +24,81 @@ public class MasterController implements Initializable {
 
     public TableView<User> tableView;
 
-
     public TableColumn<User, Integer> colID;
+    public TableColumn<User, String> colUsername;
     public TableColumn<User, String> colFirstName;
     public TableColumn<User, String> colLastName;
-    public TableColumn<User, String> colUsername;
     public TableColumn<User, String> colPassword;
     public TableColumn<User, String> colEmail;
 
     public Button closeButton;
+    public TextField textFieldUsername;
     public TextField textFieldFirstName;
     public TextField textFieldLastName;
-    public TextField textFieldUsername;
     public TextField textFieldPassword;
     public TextField textFieldEmail;
 
+    ObservableList<User> observableList = FXCollections.observableArrayList();
+
+    /*
+    Initializes the MasterController Class.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        loadData();
+
+        /* Tableview Settings */
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        // Set the cells in the table to editable
+        tableView.setEditable(true);
+        colUsername.setCellFactory(TextFieldTableCell.forTableColumn());
+        colFirstName.setCellFactory(TextFieldTableCell.forTableColumn());
+        colLastName.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPassword.setCellFactory(TextFieldTableCell.forTableColumn());
+        colEmail.setCellFactory(TextFieldTableCell.forTableColumn());
+
+    }
+    
+    /*
+    This method will return an ObservableList of User objects.
+     */
+    public void loadData() {
+        colID.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        colUsername.setCellValueFactory(new PropertyValueFactory<>("Username"));
+        colFirstName.setCellValueFactory(new PropertyValueFactory<>("Firstname"));
+        colLastName.setCellValueFactory(new PropertyValueFactory<>("Lastname"));
+        colPassword.setCellValueFactory(new PropertyValueFactory<>("Password"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("Email"));
+
+        try {
+            Connection connection = Database.getConnection();
+
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM user");
+
+            while (resultSet.next()) {
+                observableList.add(new User(
+                        resultSet.getInt("USER_ID"),
+                        resultSet.getString("USERNAME"),
+                        resultSet.getString("FIRSTNAME"),
+                        resultSet.getString("LASTNAME"),
+                        resultSet.getString("PASSWORD"),
+                        resultSet.getString("EMAIL")
+                ));
+                tableView.setItems(observableList);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(MasterController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 
     @FXML
-    public void handleCloseButtonAction(ActionEvent event) throws IOException {
+    public void buttonClose(ActionEvent event) throws IOException {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
     }
-
-    ObservableList<User> observableList = FXCollections.observableArrayList();
-
 
     @FXML
     public void buttonDeleteAll(ActionEvent event) {
@@ -64,76 +115,8 @@ public class MasterController implements Initializable {
         // Clears user table
         allUser.clear();
 
-        try {
-            Connection connection = Database.getConnection();
-
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM user");
-
-            while(resultSet.next()) {
-                observableList.add(new User(
-                        resultSet.getInt("USER_ID"),
-                        resultSet.getString("FIRSTNAME"),
-                        resultSet.getString("LASTNAME"),
-                        resultSet.getString("USERNAME"),
-                        resultSet.getString("PASSWORD"),
-                        resultSet.getString("EMAIL")
-                ));
-                tableView.setItems(observableList);
-            }
-        }
-        catch(SQLException e) {
-            Logger.getLogger(MasterController.class.getName()).log(Level.SEVERE, null, e);
-        }
-    }
-
-    /*
-    Initializes the MasterController Class.
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        /*
-        This method will return an ObservableList of User objects.
-         */
-
-        colID.setCellValueFactory(new PropertyValueFactory<>("Id"));
-        colFirstName.setCellValueFactory(new PropertyValueFactory<>("Firstname"));
-        colLastName.setCellValueFactory(new PropertyValueFactory<>("Lastname"));
-        colUsername.setCellValueFactory(new PropertyValueFactory<>("Username"));
-        colPassword.setCellValueFactory(new PropertyValueFactory<>("Password"));
-        colEmail.setCellValueFactory(new PropertyValueFactory<>("Email"));
-
-        try {
-            Connection connection = Database.getConnection();
-
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM user");
-
-            while(resultSet.next()) {
-                observableList.add(new User(
-                        resultSet.getInt("USER_ID"),
-                        resultSet.getString("FIRSTNAME"),
-                        resultSet.getString("LASTNAME"),
-                        resultSet.getString("USERNAME"),
-                        resultSet.getString("PASSWORD"),
-                        resultSet.getString("EMAIL")
-                ));
-                tableView.setItems(observableList);
-            }
-        }
-        catch(SQLException e) {
-            Logger.getLogger(MasterController.class.getName()).log(Level.SEVERE, null, e);
-        }
-
-        // Set the cells in the table to editable
-        tableView.setEditable(true);
-        colUsername.setCellFactory(TextFieldTableCell.forTableColumn());
-        colFirstName.setCellFactory(TextFieldTableCell.forTableColumn());
-        colLastName.setCellFactory(TextFieldTableCell.forTableColumn());
-        colEmail.setCellFactory(TextFieldTableCell.forTableColumn());
-        colPassword.setCellFactory(TextFieldTableCell.forTableColumn());
-
+        // Load data from database
+        loadData();
     }
 
     /*
@@ -143,15 +126,14 @@ public class MasterController implements Initializable {
     public void buttonCreateUser(ActionEvent event) {
         String encryptPass = PasswordEncryption.createHash(textFieldPassword.getText());
         User user = new User(
+                textFieldUsername.getText(),
                 textFieldFirstName.getText(),
                 textFieldLastName.getText(),
-                textFieldUsername.getText(),
                 encryptPass,
                 textFieldEmail.getText()
         );
         tableView.getItems().add(user);
         Database.registerUser(user);
-
     }
 
     /*
@@ -170,7 +152,7 @@ public class MasterController implements Initializable {
         // the selected row(s) will be deleted from allUser
         singleUser.forEach(allUser::remove);
         User user = tableView.getSelectionModel().getSelectedItem();
-        System.out.println("User: "+singleUser);
+        System.out.println("User: " + singleUser);
         Database.deleteUser(user.getId());
     }
 
@@ -183,8 +165,8 @@ public class MasterController implements Initializable {
         user.setUsername(userStringCellEditEvent.getNewValue());
         user.setFirstname(userStringCellEditEvent.getNewValue());
         user.setLastname(userStringCellEditEvent.getNewValue());
-        user.setEmail(userStringCellEditEvent.getNewValue());
         user.setPassword(userStringCellEditEvent.getNewValue());
+        user.setEmail(userStringCellEditEvent.getNewValue());
         Database.editUser(user);
     }
 
